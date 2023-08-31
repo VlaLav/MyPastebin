187d3c0ceca57659cab76920ea9eb7b074330aca
Влад <saleev.vlad11@gmail.com>
2023-08-31 12:59:07 +0300

    Add annotation to Paste.java

--- src/main/java/com/vladsaleev/mypastebin/entity/Paste.java
+++ src/main/java/com/vladsaleev/mypastebin/entity/Paste.java
@@ -11,6 +11,7 @@ import java.time.LocalDateTime;
 @Entity
 @AllArgsConstructor
 @NoArgsConstructor
+@Table(name = "paste")
 public class Paste {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
