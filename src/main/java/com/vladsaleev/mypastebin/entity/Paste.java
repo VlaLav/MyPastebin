44829600f22863acdf8ff40feb25a1e9b0b38815
Влад <saleev.vlad11@gmail.com>
2023-08-31 12:56:50 +0300

    Created entity

---
+++ src/main/java/com/vladsaleev/mypastebin/entity/Paste.java
@@ -0,0 +1,25 @@
+package com.vladsaleev.mypastebin.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+
+@Data
+@Entity
+@AllArgsConstructor
+@NoArgsConstructor
+public class Paste {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(nullable = false, updatable = false)
+    private int id;
+    @Column(nullable = false)
+    private String text;
+    @Column(nullable = false)
+    private String status;
+    private LocalDateTime expiredTime;
+    private String hash;
+}
