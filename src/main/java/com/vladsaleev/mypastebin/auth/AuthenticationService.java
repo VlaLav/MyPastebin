3a72ac62142ae99eeefe69437b55c808ebee42ea
Влад <saleev.vlad11@gmail.com>
2023-09-07 21:36:51 +0300

    Created Controller and services for auth, and created request and response classes

---
+++ src/main/java/com/vladsaleev/mypastebin/auth/AuthenticationService.java
@@ -0,0 +1,12 @@
+package com.vladsaleev.mypastebin.auth;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class AuthenticationService {
+    public AuthenticationResponse register(RegisterRequest request) {
+    }
+
+    public AuthenticationResponse authenticate(AuthenticationRequest request) {
+    }
+}
--- src/main/java/com/vladsaleev/mypastebin/controller/PasteController.java
+++ src/main/java/com/vladsaleev/mypastebin/controller/PasteController.java
@@ -14,6 +14,7 @@ import java.util.List;
 
 @RestController
 @AllArgsConstructor
+@RequestMapping("/api/v1/paste")
 public class PasteController {
 
     private PasteServiceImpl pasteService;
