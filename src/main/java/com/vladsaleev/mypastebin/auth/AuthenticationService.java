fe4707023086bd6236daefc41761664bd76b0ca3
Влад <saleev.vlad11@gmail.com>
2023-09-07 21:49:08 +0300

    Created ApplicationConfig.java

--- src/main/java/com/vladsaleev/mypastebin/auth/AuthenticationService.java
+++ src/main/java/com/vladsaleev/mypastebin/auth/AuthenticationService.java
@@ -1,12 +1,16 @@
 package com.vladsaleev.mypastebin.auth;
 
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
 @Service
+@RequiredArgsConstructor
 public class AuthenticationService {
     public AuthenticationResponse register(RegisterRequest request) {
+        return null;
     }
 
     public AuthenticationResponse authenticate(AuthenticationRequest request) {
+        return null;
     }
 }
